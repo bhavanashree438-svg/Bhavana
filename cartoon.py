b61eb8ed79aa4eadcaafef57300e7318c576b158
Bhavana shree S <bhavanashree438@gmail.com>
2026-01-29 12:07:10 +0530

    Add popular cartoon names list and print function
    Added a list of popular cartoon names and printed them.

---
+++ cartoon.py
@@ -0,0 +1,12 @@
+cartoon_names = [
+    "Doraemon",
+    "Tom and Jerry",
+    "Mickey Mouse",
+    "Shinchan",
+    "Pikachu"
+]
+
+print("Popular Cartoon Names:")
+for cartoon in cartoon_names:
+    print("-", cartoon)
+
